3531d675b6439d1d2d7be5292176d43ac7d063dc
KimTaeGwan <chtaeryu@gmail.com>
2019-10-08 13:05:03 +0900

    modify Base Station Installation.py

--- LV_3/Base Station Installation.py
+++ LV_3/Base Station Installation.py
@@ -1,9 +1,9 @@
 # https://programmers.co.kr/learn/courses/30/lessons/12979
 
 # Input Example
-n = 11              # the number of apartments
-stations = [4,11]   # base station
-w = 1               # totalW
+n = 10              # the number of apartments
+stations = [5]   # base station
+w = 4               # totalW
 # answer = 3
 
 
@@ -18,11 +18,14 @@ for station in stations:
 
 maxW = (2*w) + 1
 totalW = 0
-for i in range(len(apart)):
+for i in range(n):
     if apart[i] == 0:
         totalW+=1
     if totalW == maxW or (apart[i] == 1 and totalW > 0):
         answer += 1
         totalW = 0
+
+if totalW > 0:
+    answer+=1
         
-#정확성/효율성: 53.7/0
+#정확성/효율성: 70.5/0
